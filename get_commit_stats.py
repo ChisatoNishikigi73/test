4e7fe8744bf2c3f94a357bbd015b55cd8377a7ca
ChisatoNishikigi73 <lxr2061895177@163.com>
2024-10-07 18:05:14 +0800

    a

--- get_commit_stats.py
+++ get_commit_stats.py
@@ -8,12 +8,14 @@ token = os.getenv('GITHUB_TOKEN')
 def get_repos(username):
     url = f'https://api.github.com/users/{username}/repos'
     response = requests.get(url, auth=HTTPBasicAuth(username, token))
+    response.raise_for_status()  # Raise an error for bad status codes
     repos = response.json()
     return [repo['name'] for repo in repos]
 
 def get_commits(username, repo):
     url = f'https://api.github.com/repos/{username}/{repo}/commits'
     response = requests.get(url, auth=HTTPBasicAuth(username, token))
+    response.raise_for_status()  # Raise an error for bad status codes
     commits = response.json()
     return commits
 
@@ -28,7 +30,7 @@ def main(username):
                 total_commits += 1
 
     with open('commit_stats.txt', 'w') as f:
-        f.write(f'Total commits: {total_commits}')
+        f.write(f'总提交次数: {total_commits}')
 
 if __name__ == '__main__':
     main(username)
